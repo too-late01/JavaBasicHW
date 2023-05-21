e4de234c7a4f352dd4225c7d02f82f61e311478e
too-late01 <sysoevalida@gmail.com>
2023-05-21 22:07:20 +0300

    домашнее задание по написанию юнит тестов + тест через наследование

---
+++ HW6_UnitTests/Demo.java
@@ -0,0 +1,14 @@
+package ru.otus.java.HW6_UnitTests;
+
+
+import ru.otus.java.HW6_UnitTests.game.*;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        Dice dice = new DiceImpl();
+        GameWinnerPrinter winnerPrinter = new GameWinnerConsolePrinter();
+        Game game = new Game(dice, winnerPrinter);
+        game.playGame(new Player("Вася"), new Player("Игорь"));
+    }
+}
