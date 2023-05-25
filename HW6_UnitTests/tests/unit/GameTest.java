95932e44c3d2446fc816d69f8653e4553fa8c6e8
too-late01 <sysoevalida@gmail.com>
2023-05-25 18:43:35 +0300

    Тест на то, является ли первый игрок победителем. наследование от GameWinnerPrinter

--- HW6_UnitTests/tests/unit/GameTest.java
+++ HW6_UnitTests/tests/unit/GameTest.java
@@ -1,13 +1,7 @@
 package ru.otus.java.HW6_UnitTests.tests.unit;
 
-
-import ru.otus.java.HW6_UnitTests.Assertions.Assertions;
 import ru.otus.java.HW6_UnitTests.game.*;
 import ru.otus.java.HW6_UnitTests.tests.unit.fakes.DiceImplStub;
-
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
 import static ru.otus.java.HW6_UnitTests.Assertions.Assertions.assertEquals;
 
 public class GameTest {
@@ -25,47 +19,33 @@ public class GameTest {
             }
         }
     }
-    public interface GameWinnerSaver {
-        String printWinner(Player winner);
-    }
+//    public interface GameWinnerSaver {
+//        String printWinner(Player winner);
+//    }
 
-    public class GameWinnerConsolePrinter implements GameWinnerSaver {
+    public class GameWinnerSaver  implements GameWinnerPrinter {
+        private Player winnerName;
         @Override
-        public String printWinner(Player winner) {
-            String winnerName = winner.getName();
+        public void printWinner(Player winner) {
+            winnerName = winner;
+        }
+        public Player saveWinner() {
             return winnerName;
         }
     }
-    private static class TestGame extends Game {
-        private String winnerName;
-        private Player winner;
-        Dice dice;
-        GameWinnerPrinter winnerPrinter;
-        public TestGame(Dice dice, GameWinnerPrinter winnerPrinter) {
-            super(dice, winnerPrinter);
-        this.dice = dice;
-        this.winnerPrinter = winnerPrinter;}
 
-        public String playGameTest (Player player1, Player player2) {
-            int player1Result = dice.roll();
-            int player2Result = dice.roll();
-
-            Player winner = (player1Result > player2Result)? player1: player2;
-            String winnerName = winner.getName();
-            return winnerName;
-        }
-
-    }
     public void  testPlayer1WinsSecond() {
         String scenario = "Проверка, что победитель - 1 игрок (через наследование)";
         try {
             Dice dice = new DiceImplStub();
-            GameWinnerPrinter winnerPrinter = new ru.otus.java.HW6_UnitTests.game.GameWinnerConsolePrinter();
-            TestGame game = new TestGame(dice, winnerPrinter);
+            GameWinnerSaver winnerPrinter = new GameWinnerSaver();
+            Game game = new Game(dice, winnerPrinter);
             Player player1 = new Player("John");
             Player player2 = new Player("Jane");
-            String expectedString = "John";
-            assertEquals(expectedString, game.playGameTest(player1, player2));
+            game.playGame(player1, player2);
+            Player winnerPlayer = winnerPrinter.saveWinner();
+            Player expectedString = new Player("John");
+            assertEquals(expectedString.getName(), winnerPlayer.getName());
             System.out.printf("\"%s\" passed %n", scenario);
         } catch (Throwable e) {
             System.err.printf("\"%s\" fails with message \"%s\" %n", scenario, e.getMessage());
